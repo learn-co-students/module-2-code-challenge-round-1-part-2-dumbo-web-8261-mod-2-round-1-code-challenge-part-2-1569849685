f8527bb258de300a80a6bbcab7105e61366b2cf8
jgibbel <jamesgibbel@gmail.com>
2019-09-30 09:59:31 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,24 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show 
+    @heroine = Heroine.find(params[:id])
+  end 
+
+  def new 
+    @heroine = Heroine.new 
+    @powers = Power.all
+  end 
+
+  def create 
+    @heroine = Heroine.create(params.require(:heroine).permit(:name, :super_name, :power_id))
+    if @heroine.valid? 
+      redirect_to heroine_path(@heroine)
+    else 
+      flash[:errors] = @heroine.errors.full_messages 
+      redirect_to new_heroine_path 
+    end 
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+    has_one :power 
+    validates :super_name, uniqueness: true
 end
